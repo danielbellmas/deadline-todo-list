1f60f3e89fef76f32ff67a3c7f9cfb4aa8c6b4cc
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2023-07-13 11:19:00 +0300

    Create a Form to create a new todo

--- src/components/todo-form/TodoForm.tsx
+++ src/components/todo-form/TodoForm.tsx
@@ -1,5 +1,7 @@
 import React, { FormEvent, useState } from 'react';
 import { Todo } from '../../types/todo';
+import { formatDateYYYYMMDD } from '../../utils/date';
+import './TodoForm.css';
 
 interface TodoFormProps {
   addTodo: (newTodo: Omit<Todo, '_id'>) => Promise<void>;
@@ -25,9 +27,9 @@ export const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="todo-form" onSubmit={handleSubmit}>
       <input type="text" value={title} placeholder="Add a todo" onChange={e => setTitle(e.target.value)} />
-      <input type="date" value={deadline} onChange={e => setDeadline(e.target.value)} />
+      <input type="date" value={deadline} onChange={e => setDeadline(formatDateYYYYMMDD(new Date(e.target.value)))} />
       <button type="submit">Add</button>
     </form>
   );
